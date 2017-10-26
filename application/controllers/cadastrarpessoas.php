e96c442235055519c2bbf1334fe8109bfc201cc7
marcelomartins <marcelorambomartins@gmail.com>
2017-10-25 22:28:29 -0200

    criado view e controller de cadastrar pessoas

---
+++ application/controllers/cadastrarpessoas.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CadastrarPessoas extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+	}
+
+
+public function index()
+	{
+		
+		$this->load->helper('form');
+		$this->load->library(array('form_validation','email'));
+		//validação do formulário
+		$this->form_validation->set_rules('name','Nome','trim|required');
+		$this->form_validation->set_rules('email','Email','trim|required|valid_email');
+		$this->form_validation->set_rules('password','Senha','trim|required');
+
+
+		if($this->form_validation->run()==FALSE):
+			$dados['formerror']=validation_errors();
+		else:
+			$dados['formerror']=NULL;
+		endif;
+
+		$this->load->view('viewCadastrarPessoas', $dados);
+
+	}
+}
+?>
