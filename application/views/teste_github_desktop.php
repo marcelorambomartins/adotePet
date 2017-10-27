17051a9517558d54e9d22ea6ae3c4880be0131eb
Richard Nunes <pagamentos.extremeglobal@gmail.com>
2017-10-27 14:14:27 -0200

    teste commit github desktop
    Description teste commit github desktop

---
+++ application/views/teste_github_desktop.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<?php
+		$this->load->view('head');
+		?>
+	</head>
+	<body>
+		<?php
+			$this->load->view('menu');
+		?>
+
+		<div class="coluna col7 login text-center">
+			<?php
+				echo form_open('login');
+				echo form_label('Email','email');
+				echo '<br>';
+				echo form_input('email', set_value('email'));
+				echo '<br>';				
+				echo form_label('Senha','senha');
+				echo '<br>';				
+				echo form_password('password', set_value('password'));
+				echo '<br>';				
+				echo form_submit('enviar', 'Enviar', array('class'=>'botao'));
+				echo form_close();
+				if($formerror):
+					echo '<div class="alert alert-danger">'.$formerror.'</div>';
+				endif;				
+			?>
+			<p> 
+				Não possui cadastro? <a href="Cadastrarpessoas">Clique aqui</a>
+			</p>			
+		</div>
+	</body>
+</html>
