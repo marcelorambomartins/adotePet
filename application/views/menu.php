3c346cd41521456604bb61490a64fddd67bd6e9d
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2017-10-30 13:27:41 -0200

    Ajustes na barra superior

--- application/views/menu.php
+++ application/views/menu.php
@@ -11,19 +11,20 @@
 	</div>
 	<div id="navbarCollapse" class="collapse navbar-collapse">
 		<ul class="nav navbar-nav">
-			<li><a href="home"><span class="glyphicon glyphicon-home"></span></a></li>';
-			<li><a href="login"><span class="fa fa-paw"></span> Animais para adoção</a></li>
+			<li><a href="../home"><span class="glyphicon glyphicon-home"></span></a></li>
+			<li><a href="../login"><span class="fa fa-paw"></span> Animais para adoção</a></li>
+			<li><a href="caes/cadastrar"><span class="fa fa-paw"></span> Cadastrar Caes</a></li>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
 			<?php
 			$usertype=0;
 			if($usertype==1):
-      		echo'<li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+      		echo'<li><a href="../logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
       		elseif($usertype==2):
-      		echo'<li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+      		echo'<li><a href="../logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
       		else:
-			echo'<li><a href="cadastrarpessoas"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>';
-      		echo'<li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
+			echo'<li><a href="../cadastrarpessoas"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>';
+      		echo'<li><a href="../login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
       		endif;
       		?>
 		</ul>
