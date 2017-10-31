890d6d30a652db1d1aeae953c9e4d85f689ceb9b
Richard Nunes <pagamentos.extremeglobal@gmail.com>
2017-10-31 19:53:06 -0200

    corrigido menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -9,20 +9,20 @@
 	</div>
 	<div id="navbarCollapse" class="collapse navbar-collapse">
 		<ul class="nav navbar-nav">
-			<li><a href="../home"><span class="glyphicon glyphicon-home"></span></a></li>
-			<li><a href="pessoas/login"><span class="fa fa-paw"></span> Animais para adoção</a></li>
-			<li><a href="../caes/cadastrar"><span class="fa fa-paw"></span> Cadastrar Caes</a></li>
+			<li><a href="http://localhost/viralate/home"><span class="glyphicon glyphicon-home"></span></a></li>
+			<li><a href="http://localhost/viralate/pessoas/login"><span class="fa fa-paw"></span> Animais para adoção</a></li>
+			<li><a href="http://localhost/viralate/caes/cadastrar"><span class="fa fa-paw"></span> Cadastrar Caes</a></li>
 		</ul>
 		<ul class="nav navbar-nav navbar-right">
 			<?php
 			$usertype=0;
 			if($usertype==1):
-      		echo'<li><a href="../logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+      		echo'<li><a href="http://localhost/viralate/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
       		elseif($usertype==2):
-      		echo'<li><a href="../logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+      		echo'<li><a href="http://localhost/viralate/logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
       		else:
-			echo'<li><a href="pessoas/cadastrar"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>';
-      		echo'<li><a href="pessoas/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
+			echo'<li><a href="http://localhost/viralate/pessoas/cadastrar"><span class="glyphicon glyphicon-user"></span> Cadastrar</a></li>';
+      		echo'<li><a href="http://localhost/viralate/pessoas/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
       		endif;
       		?>
 		</ul>
