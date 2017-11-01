0f96465a5a40706f2ca34560c36c50cf31a17d7a
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2017-11-01 21:45:25 -0200

    HTML estrutura PerfilCao

--- application/views/viewPerfilCao.php
+++ application/views/viewPerfilCao.php
@@ -10,25 +10,38 @@
 		?>
 	<div class="container">
 
-		<div class="row">
-			<div class="col-sm-12">
-			<div class="panel panel-default text-center">
-				<h3 >Filtros</h3>
-			</div>
-			</div>
-		</div>
+		<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Welcome</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <hr>
+      <h3>Test</h3>
+      <p>Lorem ipsum...</p>
+    </div>
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>ADS</p>
+      </div>
+      <div class="well">
+        <p>ADS</p>
+      </div>
+    </div>
+  </div>
+</div>
 
-		<div class="container-fluid">
 
-			<div class="row"><!--inicio da row-->
-
-			
-
-			</div> <!-- fim da div row-->
-
-		</div>
 		
 	</div>
+
+	<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
 
 	</body>
 </html>
