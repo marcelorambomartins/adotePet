a04d0058ff021c9e02d8552f9d8cc16f9c904e48
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2017-11-05 00:02:12 -0200

    Criado mensagens de alerta ao buscar caes

--- application/views/listagemcaes.php
+++ application/views/listagemcaes.php
@@ -12,44 +12,60 @@
 
 		<div class="row">
 			<div class="col-sm-12">
-			<div class="panel panel-default text-left">			
-			<?php
-			if (isset($castrado)){
-				$castrado = TRUE;
-			}else{
-				$castrado = FALSE;
-			}
-			if (isset($vacinado)){
-				$vacinado = TRUE;
-			}else{
-				$vacinado = FALSE;
-			}
-			if (isset($adotado)){
-				$adotado = TRUE;
-			}else{
-				$adotado = FALSE;
-			}
-			echo form_open('http://localhost/viralate/caes/filtrar');
-			echo form_checkbox('castrado', 'sim', $castrado);
-			echo form_label('Castrado', 'castrado')."&nbsp&nbsp&nbsp&nbsp";
-			echo form_checkbox('vacinado', 'sim', $vacinado);
-			echo form_label('Vacinado', 'vacinado')."&nbsp&nbsp&nbsp&nbsp";			
-			echo form_checkbox('adotado', 'sim', $adotado);
-			echo form_label('Adotado', 'adotado')."&nbsp&nbsp&nbsp&nbsp";				
-			
+				<div class="panel panel-default text-left">			
+					<?php
+						if (isset($castrado)){
+							$castrado = TRUE;
+						}else{
+							$castrado = FALSE;
+						}
+						if (isset($vacinado)){
+							$vacinado = TRUE;
+						}else{
+							$vacinado = FALSE;
+						}
+						if (isset($adotado)){
+							$adotado = TRUE;
+						}else{
+							$adotado = FALSE;
+						}
 
-			echo '<button type="submit"><span class="glyphicon glyphicon-search"></span><span class="submit-text"></span></button>';
+						$atributos = array('id' => 'filtros');
+						echo form_open('http://localhost/viralate/caes/filtrar',$atributos);
+						echo form_checkbox('castrado', 'sim', $castrado);
+						echo form_label('Castrado', 'castrado')."&nbsp&nbsp&nbsp&nbsp";
+						echo form_checkbox('vacinado', 'sim', $vacinado);
+						echo form_label('Vacinado', 'vacinado')."&nbsp&nbsp&nbsp&nbsp";			
+						echo form_checkbox('adotado', 'sim', $adotado);
+						echo form_label('Adotado', 'adotado')."&nbsp&nbsp&nbsp&nbsp";				
+						
+
+						echo '<button type="submit"><span class="glyphicon glyphicon-search"></span><span class="submit-text"></span></button>';
 
 
-			echo form_close();			
-			?>
-			</div>
+						echo form_close();			
+					?>
+				</div>
 			</div>
 		</div>
 
 		<div class="container-fluid">
 
 			<div class="row"><!--inicio da row-->
+
+			<div class="alert alert-info text-center">
+				<?php
+					if($listacaes == null){
+						echo "Sem Resultados";
+					}else if($castrado == false and $vacinado == false and $adotado == false){
+						echo "Todos os Resultados";
+					}else{
+						echo "Resultados para a busca";
+					}
+  				
+  				?>
+			</div>
+
 
 			<?php
 				foreach ($listacaes as $cao) {
