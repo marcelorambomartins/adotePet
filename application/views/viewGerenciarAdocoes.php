4af7b2d3e1000ff5e0d009ec77be8541ad856c85
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2017-11-06 00:34:53 -0200

    Criado as View de gerenciar usuario e Gerenciar Adoções

---
+++ application/views/viewGerenciarAdocoes.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<head>
+		<?php
+		$this->load->view('head');
+		?>
+	</head>
+	<body>
+		<?php
+			$this->load->view('menu');
+		?>
+	<div class="container">
+
+      <table class="table">
+      </table>
+		
+	</div>
+
+
+	</body>
+</html>
